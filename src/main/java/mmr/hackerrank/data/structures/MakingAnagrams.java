daac2a3d60dcb64a37a41dc4b0d695814be56fd1
Muhammed Mashiur Rahman <muhammed.rahman@originsystems.co.za>
2018-05-16 16:16:58 +0200

    init

---
+++ src/main/java/mmr/hackerrank/data/structures/MakingAnagrams.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package mmr.hackerrank.data.structures;
+
+/**
+ * @author muhammed.rahman (oncmmr@gmail.com)
+ *
+ */
+public class MakingAnagrams {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
