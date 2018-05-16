55a6f641df417768674e766ec3cf9777fec5866b
Muhammed Mashiur Rahman <muhammed.rahman@originsystems.co.za>
2018-05-16 12:05:53 +0200

    implement arrayLeftRotation method - which does left rotation

--- mmr/hackerrank/ArraysLeftRotation.java
+++ mmr/hackerrank/ArraysLeftRotation.java
@@ -3,6 +3,8 @@
  */
 package mmr.hackerrank;
 
+import java.util.Scanner;
+
 /**
  * @author muhammed.rahman (oncmmr@gmail.com)
  *
@@ -13,8 +15,40 @@ public class ArraysLeftRotation {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int k = in.nextInt();
+        int a[] = new int[n];
+        for(int a_i=0; a_i < n; a_i++){
+            a[a_i] = in.nextInt();
+        }
+      
+        in.close();
+        
+        int[] output = new int[n];
+        output = arrayLeftRotation(a, n, k);
+        for(int i = 0; i < n; i++)
+            System.out.print(output[i] + " ");
+      
+        System.out.println();
+      
+    }
+	/**
+	 * 
+	 * @param array a
+	 * @param int n
+	 * @param int k
+	 * @return array
+	 */
+	private final static int[] arrayLeftRotation(int[] a, int n, int k) {		
+		for(int i = 0; i < k; i++){
+			int first = a[0];
+			for(int j = 0; j < n-1; j++){				
+				a[j]=a[j+1];
+			}
+			a[n-1] = first;
+		}
+		return a;
+    }
 
 }
