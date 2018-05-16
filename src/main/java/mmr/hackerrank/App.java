83e2812066602538deb4d4aabac052c9dbcb0b45
Muhammed Mashiur Rahman <muhammed.rahman@originsystems.co.za>
2018-05-16 15:00:43 +0200

    some init files

---
+++ src/main/java/mmr/hackerrank/App.java
@@ -0,0 +1,13 @@
+package mmr.hackerrank;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
