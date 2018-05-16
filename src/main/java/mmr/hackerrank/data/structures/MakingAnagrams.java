8abea678f90158442b45db6d4fd3ab572adccf18
Muhammed Mashiur Rahman <muhammed.rahman@originsystems.co.za>
2018-05-16 16:48:58 +0200

    converting Input string to array

--- src/main/java/mmr/hackerrank/data/structures/MakingAnagrams.java
+++ src/main/java/mmr/hackerrank/data/structures/MakingAnagrams.java
@@ -3,6 +3,11 @@
  */
 package mmr.hackerrank.data.structures;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Scanner;
+
 /**
  * @author muhammed.rahman (oncmmr@gmail.com)
  *
@@ -12,9 +17,20 @@ public class MakingAnagrams {
 	/**
 	 * @param args
 	 */
+	private static final Scanner scanner = new Scanner(System.in);
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		String a = scanner.nextLine();
+		char[] chars1 = a.toLowerCase().toCharArray();
+        Arrays.sort(chars1);
+		
+        String b = scanner.nextLine();
+        char[] chars2 = b.toLowerCase().toCharArray();
+        Arrays.sort(chars2);
+		
+        scanner.close();
+	}	
 
-	}
 
 }
