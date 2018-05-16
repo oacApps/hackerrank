e17d455c446f44304fc586d60233b862ece6a2b6
Muhammed Mashiur Rahman <muhammed.rahman@originsystems.co.za>
2018-05-16 11:56:02 +0200

    First init of ArraysLeftRotation

---
+++ mmr/hackerrank/ArraysLeftRotation.java
@@ -0,0 +1,20 @@
+/**
+ * Problem: https://github.com/oacApps/hackerrank/issues/1
+ */
+package mmr.hackerrank;
+
+/**
+ * @author muhammed.rahman (oncmmr@gmail.com)
+ *
+ */
+public class ArraysLeftRotation {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
